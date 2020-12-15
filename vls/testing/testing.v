1eb2d92140f969c6bd81a88aa6986d52513c00cf
Daniel Däschle <daniel.daeschle@gmail.com>
2020-12-15 09:42:01 +0100

    vls: use interface instead of anonymous function

---
+++ vls/testing/testing.v
@@ -0,0 +1,14 @@
+module testing
+
+pub struct Testio {
+pub mut:
+	response string
+}
+
+pub fn (mut io Testio) send(data string) {
+	io.response = data
+}
+
+pub fn (io Testio) receive() string {
+	return ''
+}
