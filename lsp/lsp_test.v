e9ad60a7f09d9d57288224184f4463520121b2d2
Ned Palacios <npdoesmc@gmail.com>
2020-12-16 15:29:15 +0800

    lsp: update comment and test

--- lsp/lsp_test.v
+++ lsp/lsp_test.v
@@ -2,9 +2,8 @@ module lsp
 
 fn test_document_uri_from_path() {
 	input := '/foo/bar/test.v'
-	// NOTE: Testing will result to a cgen error when not explictly casted to string. 
-	expected := document_uri_from_path(input).str()
-	assert expected == 'file:///foo/bar/test.v'
+	// TODO: remove str() after https://github.com/vlang/v/issues/7354 is fixed. 
+	assert document_uri_from_path(input).str() == 'file:///foo/bar/test.v'
 }
 
 fn test_document_uri_path() {
