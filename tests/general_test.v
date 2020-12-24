667dc63a822050487128c5a3dad33ca805fd2022
Daniel Däschle <daniel.daeschle@gmail.com>
2020-12-24 10:31:58 +0100

    vls: rename execute to dispatch (#37)

--- tests/general_test.v
+++ tests/general_test.v
@@ -5,7 +5,7 @@ fn test_wrong_first_request() {
 	mut io := testing.Testio{}
 	payload := '{"jsonrpc":"2.0","id":1,"method":"shutdown","params":{}}'
 	mut ls := vls.new(io)
-	ls.execute(payload)
+	ls.dispatch(payload)
 	status := ls.status()
 	assert status == .off
 	assert io.response == '{"jsonrpc":"2.0","id":0,"error":{"code":-32002,"message":"Server not yet initialized.","data":""},"result":""}'
@@ -15,7 +15,7 @@ fn test_initialize_with_capabilities() {
 	mut io := testing.Testio{}
 	payload := '{"jsonrpc":"2.0","id":1,"method":"initialize","params":{}}'
 	mut ls := vls.new(io)
-	ls.execute(payload)
+	ls.dispatch(payload)
 	status := ls.status()
 	assert status == .initialized
 	assert io.response == '{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":1,"hoverProvider":false,"completionProvider":{"resolveProvider":false,"triggerCharacters":[]},"signatureHelpProvider":{"triggerCharacters":[],"retriggerCharacters":[]},"definitionProvider":false,"typeDefinitionProvider":false,"implementationProvider":false,"referencesProvider":false,"documentHightlightProvider":false,"documentSymbolProvider":false,"workspaceSymbolProvider":false,"codeActionProvider":false,"codeLensProvider":{"resolveProvider":false},"documentFormattingProvider":false,"documentOnTypeFormattingProvider":{"moreTriggerCharacter":[]},"renameProvider":false,"documentLinkProvider":false,"colorProvider":false,"declarationProvider":false,"executeCommandProvider":"","experimental":{}}}}'
@@ -24,7 +24,7 @@ fn test_initialize_with_capabilities() {
 fn test_initialized() {
 	payload := '{"jsonrpc":"2.0","id":1,"method":"initialized","params":{}}'
 	mut ls := init()
-	ls.execute(payload)
+	ls.dispatch(payload)
 	status := ls.status()
 	assert status == .initialized
 }
@@ -32,7 +32,7 @@ fn test_initialized() {
 fn test_shutdown() {
 	payload := '{"jsonrpc":"2.0","method":"shutdown","params":{}}'
 	mut ls := init()
-	ls.execute(payload)
+	ls.dispatch(payload)
 	status := ls.status()
 	assert status == .shutdown
 }
@@ -41,6 +41,6 @@ fn init() vls.Vls {
 	mut io := testing.Testio{}
 	payload := '{"jsonrpc":"2.0","id":1,"method":"initialize","params":{}}'
 	mut ls := vls.new(io)
-	ls.execute(payload)
+	ls.dispatch(payload)
 	return ls
 }
