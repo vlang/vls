44b1c67c40d60d1198ab48b6d0746012888934c5
Ned Palacios <npdoesmc@gmail.com>
2020-12-26 19:11:25 +0800

    vls: use []byte when storing source

--- vls/diagnostics.v
+++ vls/diagnostics.v
@@ -5,9 +5,10 @@ import json
 import jsonrpc
 import v.token
 import v.util
+import v.ast
 
 // get_column computes the column of the source based on the given initial position
-fn get_column(source string, init_pos int) int {
+fn get_column(source []byte, init_pos int) int {
 	mut p := init_pos
 	if source.len > 0 {
 		for ; p >= 0; p-- {
@@ -20,7 +21,7 @@ fn get_column(source string, init_pos int) int {
 }
 
 // position_to_lsp_pos converts the token.Position into lsp.Position
-pub fn position_to_lsp_pos(source string, pos token.Position) lsp.Position {
+pub fn position_to_lsp_pos(source []byte, pos token.Position) lsp.Position {
 	p := util.imax(0, util.imin(source.len - 1, pos.pos))
 	column := util.imax(0, pos.pos - get_column(source, p)) - 1
 	return lsp.Position{
@@ -30,7 +31,7 @@ pub fn position_to_lsp_pos(source string, pos token.Position) lsp.Position {
 }
 
 // position_to_lsp_pos converts the token.Position into lsp.Range
-fn position_to_lsp_range(source string, pos token.Position) lsp.Range {
+fn position_to_lsp_range(source []byte, pos token.Position) lsp.Range {
 	start_pos := position_to_lsp_pos(source, pos)
 	return lsp.Range{
 		start: start_pos
@@ -42,9 +43,9 @@ fn position_to_lsp_range(source string, pos token.Position) lsp.Range {
 }
 
 // show_diagnostics converts the file ast's errors and warnings and publishes them to the editor
-fn (ls Vls) show_diagnostics(source string, uri lsp.DocumentUri) {
+fn (ls Vls) show_diagnostics(file ast.File, source []byte) {
+	uri := lsp.document_uri_from_path(file.path)
 	mut diagnostics := []lsp.Diagnostic{}
-	file := ls.files[uri.str()]
 	for _, error in file.errors {
 		diagnostics << lsp.Diagnostic{
 			range: position_to_lsp_range(source, error.pos)
