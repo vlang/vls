7ea120622a5dff8f6c3bfc23ce549b9bac919b42
Ned Palacios <npdoesmc@gmail.com>
2021-01-10 14:50:18 +0800

    vls: add custom ast walker function

---
+++ vls/ast.v
@@ -0,0 +1,31 @@
+module vls
+
+import v.ast
+import v.ast.walker
+
+struct FindNodeByPos {
+	pos  int
+mut:
+	node ast.Node
+}
+
+pub fn find_ast_by_pos(nodes []ast.Node, offset int) ?ast.Node {
+	mut data := FindNodeByPos{
+		pos: offset
+	}
+	for node in nodes {
+		walker.inspect(node, data, fn (node ast.Node, mut data FindNodeByPos) bool {
+			pos := data.pos
+			node_pos := node.position()
+			if pos >= node_pos.pos && pos <= node_pos.pos + node_pos.len {
+				data.node = node
+				return false
+			}
+			return true
+		})
+	}
+	if isnil(data.node) {
+		return error('not found')
+	}
+	return data.node
+}
