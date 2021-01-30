174483b43bb3f88be794699e374d333c85a9f4e5
Ned Palacios <npdoesmc@gmail.com>
2021-01-30 21:27:57 +0800

    vls: add selectorexpr cond (from improve-testing pr)

--- vls/ast.v
+++ vls/ast.v
@@ -17,6 +17,13 @@ pub fn find_ast_by_pos(nodes []ast.Node, offset int) ?ast.Node {
 		walker.inspect(node, data, fn (node ast.Node, mut data FindNodeByPos) bool {
 			pos := data.pos
 			node_pos := node.position()
+			if node is ast.Expr {
+				expr := node
+				if node is ast.SelectorExpr {
+					data.node = expr
+					return false
+				}
+			}
 			if pos >= node_pos.pos && pos <= node_pos.pos + node_pos.len {
 				data.node = node
 				return false
