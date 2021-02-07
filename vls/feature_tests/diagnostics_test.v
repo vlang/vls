baea0ca8dff9e02a4d632b46feb32ac1de4506ce
Ned Palacios <npdoesmc@gmail.com>
2021-02-07 13:38:56 +0800

    vls/tests: decode params; use struct comparison instead

--- vls/feature_tests/diagnostics_test.v
+++ vls/feature_tests/diagnostics_test.v
@@ -48,10 +48,8 @@ fn test_diagnostics() {
 		req, _ := io.open_document(file_path, content)
 		ls.dispatch(req)
 	}
-	method, params := io.notification() or {
-		assert false
-		return
-	}
+	method, params := io.notification() or { '', '{}' }
+	diagnostic_params := json.decode(lsp.PublishDiagnosticsParams, params) or { lsp.PublishDiagnosticsParams{} }
 	assert method == 'textDocument/publishDiagnostics'
-	assert params == json.encode(diagnostics_result)
+	assert diagnostic_params == diagnostics_result
 }
