c69fca4d739b4261907fb12973805dad120b45c6
Ned Palacios <npdoesmc@gmail.com>
2021-03-03 10:30:00 +0800

    lsp/log: make log kind an enum

--- lsp/log/log.v
+++ lsp/log/log.v
@@ -22,15 +22,6 @@ pub mut:
 	cur_requests map[int]string = map[int]string{}
 }
 
-const (
-	send_notification = 'send-notification'
-	recv_notification = 'recv-notification'
-	send_request      = 'send-request'
-	recv_request      = 'recv-request'
-	send_response     = 'send-response'
-	recv_response     = 'recv-response'
-)
-
 pub enum TransportKind {
 	send
 	receive
@@ -43,8 +34,28 @@ struct Payload {
 	params string [raw]
 }
 
+enum LogKind {
+	send_notification
+	recv_notification
+	send_request
+	recv_request
+	send_response
+	recv_response
+}
+
+pub fn (lk LogKind) str() string {
+	return match lk {
+		.send_notification { 'send-notification' }
+		.recv_notification { 'recv-notification' }
+		.send_request { 'send-request' }
+		.recv_request { 'recv-request' }
+		.send_response { 'send-response' }
+		.recv_response { 'recv-response' }
+	}
+}
+
 pub struct LogItem {
-	@type     string
+	kind      LogKind
 	message   string
 	method    string
 	timestamp time.Time // unix timestamp
@@ -110,9 +121,9 @@ fn (mut l Log) write(item LogItem) {
 }
 
 pub fn (mut l Log) request(msg string, kind TransportKind) {
-	kind_str := match kind {
-		.send { log.send_request }
-		.receive { log.recv_request }
+	req_kind := match kind {
+		.send { LogKind.send_request }
+		.receive { LogKind.recv_request }
 	}
 
 	mut req_method := ''
@@ -124,25 +135,25 @@ pub fn (mut l Log) request(msg string, kind TransportKind) {
 		l.cur_requests.delete(payload.id.str())
 	}
 
-	l.write(@type: kind_str, message: msg, method: req_method, timestamp: time.now())
+	l.write(kind: req_kind, message: msg, method: req_method, timestamp: time.now())
 }
 
 pub fn (mut l Log) response(msg string, kind TransportKind) {
-	kind_str := match kind {
-		.send { log.send_response }
-		.receive { log.recv_response }
+	resp_kind := match kind {
+		.send { LogKind.send_response }
+		.receive { LogKind.recv_response }
 	}
 
-	l.write(@type: kind_str, message: msg, timestamp: time.now())
+	l.write(kind: resp_kind, message: msg, timestamp: time.now())
 }
 
 pub fn (mut l Log) notification(msg string, kind TransportKind) {
-	kind_str := match kind {
-		.send { log.send_notification }
-		.receive { log.recv_notification }
+	notif_kind := match kind {
+		.send { LogKind.send_notification }
+		.receive { LogKind.recv_notification }
 	}
 
-	l.write(@type: kind_str, message: msg, timestamp: time.now())
+	l.write(kind: notif_kind, message: msg, timestamp: time.now())
 }
 
 fn (li LogItem) encode(format Format) string {
@@ -153,7 +164,7 @@ fn (li LogItem) encode(format Format) string {
 }
 
 pub fn (li LogItem) json() string {
-	return '{"kind":"${li.@type}","message":${li.message},"timestamp":${li.timestamp.unix}}'
+	return '{"kind":"${li.kind}","message":${li.message},"timestamp":${li.timestamp.unix}}'
 }
 
 pub fn (li LogItem) text() string {
