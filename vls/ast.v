2b7395ead3e24126193130d4b22bbd3783086371
Ned Palacios <npdoesmc@gmail.com>
2021-03-12 19:15:07 +0800

    vls: fix find_ast_by_pos

--- vls/ast.v
+++ vls/ast.v
@@ -6,7 +6,8 @@ import v.ast.walker
 struct FindNodeByPos {
 	pos int
 mut:
-	node ast.Node
+	found bool
+	node  ast.Node
 }
 
 pub fn find_ast_by_pos(nodes []ast.Node, offset int) ?ast.Node {
@@ -14,23 +15,29 @@ pub fn find_ast_by_pos(nodes []ast.Node, offset int) ?ast.Node {
 		pos: offset
 	}
 	for node in nodes {
+		if data.found {
+			return data.node
+		}
 		walker.inspect(node, data, fn (node ast.Node, mut data FindNodeByPos) bool {
 			node_pos := node.position()
-			if node is ast.Expr {
-				expr := node
-				match node {
-					ast.SelectorExpr, ast.CallExpr {
-						data.node = expr
-						return false
-					}
-					else {}
-				}
-			}
 			if is_within_pos(data.pos, node_pos) {
 				data.node = node
-				return false
+				data.found = true
+			} else if data.pos - node_pos.pos <= 5 {
+				if node is ast.Expr {
+					expr := node
+					match node {
+						ast.SelectorExpr, ast.CallExpr {
+							data.node = expr
+							data.found = true
+						}
+						else {}
+					}
+				} else if node is ast.Stmt {
+					data.found = node is ast.Import
+				}
 			}
-			return true
+			return if data.found { false } else { true }
 		})
 	}
 	if isnil(data.node) {
