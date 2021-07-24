73f216590d1d11ceaca8ed52ec172b83c37ecfe2
Ned Palacios <npdoesmc@gmail.com>
2021-07-24 17:19:25 +0800

    vls: close typing_ch channel on test

--- vls/vls.v
+++ vls/vls.v
@@ -103,7 +103,7 @@ pub fn new(io ReceiveSender) Vls {
 	mut parser := tree_sitter.new_parser()
 	parser.set_language(v.language)
 
-	return Vls{
+	inst := Vls{
 		io: io
 		parser: parser
 		debug: io.debug
@@ -112,6 +112,12 @@ pub fn new(io ReceiveSender) Vls {
 			default_import_paths: [vlib_path, vmodules_path]
 		}
 	}
+
+	$if test {
+		inst.typing_ch.close()	
+	}
+
+	return inst
 }
 
 pub fn (mut ls Vls) dispatch(payload string) {
