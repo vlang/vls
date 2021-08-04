cf35c0be46453d72bab31ee4f16a5c45ed594e07
Ned Palacios <npdoesmc@gmail.com>
2021-08-04 19:06:48 +0800

    analyzer: fix setting symbol access to method

--- analyzer/symbol_registration.v
+++ analyzer/symbol_registration.v
@@ -287,9 +287,9 @@ fn (mut sr SymbolRegistration) fn_decl(fn_node C.TSNode) ?&Symbol {
 		if receivers.len != 0 {
 			mut parent := receivers[0].return_type
 			if !isnil(parent) && !parent.is_void() {
-				// eprintln('adding ${fn_sym.name} to ${parent.kind} ${parent.gen_str()} ${parent.is_void()}')
-				if parent.access == .private_mutable {
-					fn_sym.access = if fn_sym.access == .private { parent.access } else { SymbolAccess.public_mutable }
+				// eprintln('adding ${fn_sym.name} to ${parent.kind} ${parent.gen_str()} ${parent.access} ${parent.is_void()}')
+				if receivers[0].access == .private_mutable {
+					fn_sym.access = if fn_sym.access == .private { receivers[0].access } else { SymbolAccess.public_mutable }
 				}
 				parent.add_child(mut fn_sym) or {}
 			}
@@ -300,13 +300,13 @@ fn (mut sr SymbolRegistration) fn_decl(fn_node C.TSNode) ?&Symbol {
 
 	// scan params
 	mut params := extract_parameter_list(params_list_node, mut sr.store, sr.src_text)
+	defer { unsafe { params.free() } }
+
 	for i := 0; i < params.len; i++ {
 		mut param := params[i]
 		fn_sym.add_child(mut param) or { continue }
 		scope.register(param) or { continue }
 	}
-
-	unsafe { params.free() }
 
 	// extract function body
 	if !body_node.is_null() && !sr.is_import {
