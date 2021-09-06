914c5337d1dc1689278f658c38d0fe5c39124b61
Ned Palacios <npdoesmc@gmail.com>
2021-09-06 13:07:00 +0800

    analyzer: fix error from last commit

--- analyzer/store.v
+++ analyzer/store.v
@@ -899,8 +899,8 @@ pub fn (mut ss Store) delete_symbol_at_node(root_node C.TSNode, src []byte, at_r
 				language := ss.symbols[ss.cur_dir][idx].language
 				if idx != -1 && idx < ss.symbols[ss.cur_dir].len {
 					if language != .v {
-						binded_location_idx := ss.binded_symbol_locations.index(name)
-						if binded_location_idx != -1 && ss.binded_symbol_locations[binded_location_idx] == ss.cur_dir {
+						binded_location_idx := ss.binded_symbol_locations.index(symbol_name)
+						if binded_location_idx != -1 && ss.binded_symbol_locations[binded_location_idx].module_path == ss.cur_dir {
 							ss.binded_symbol_locations.delete(binded_location_idx)
 						}
 					}
