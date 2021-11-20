d15dc9b191a17dd3077b2efa9d828877704cbea6
Ned <7358345+nedpals@users.noreply.github.com>
2021-11-20 11:19:44 +0800

    server, lsp/log: update log file creation handling (#274)
    Closes #272 . It makes set_logpath return an option instead of panicing. It also updates the setup_logger method and refactor some parts of general.v
    
    * lsp/log: make set_logpath return an optional
    
    * server: remove manualfree attribute to process_builtin
    
    * server: separate logic for printing info and setting up logger
    
    * server: add log file handling test
    
    * server: use setup_logger in panic method
    
    * server: fmt
    
    * server: fmt general_test.v
    
    * server: fix general_test failing in windows

--- server/general_test.v
+++ server/general_test.v
@@ -2,6 +2,7 @@ import server
 import test_utils
 import lsp
 import json
+import os
 
 fn test_wrong_first_request() ? {
 	mut io := &test_utils.Testio{}
@@ -77,6 +78,28 @@ fn test_set_features() {
 		assert err.msg == 'feature "logging" not found'
 		return
 	}
+}
+
+fn test_setup_logger() ? {
+	println('test_setup_logger')
+	mut io := &test_utils.Testio{
+		debug: true
+		max_nr_responses: 10
+	}
+	mut ls := server.new(io)
+	ls.dispatch(io.request_with_params('initialize', lsp.InitializeParams{
+		root_uri: lsp.document_uri_from_path(os.join_path('non_existent', 'path'))
+	}))
+
+	method, params := io.notification_at_index(0) ?
+	assert method == 'window/showMessage'
+
+	expected_err_path := os.join_path('non_existent', 'path', 'vls.log').replace(r'\',
+		r'\\')
+	alt_log_filename := 'vls__non_existent_path.log'
+	expected_alt_log_path := os.join_path(os.home_dir(), alt_log_filename).replace(r'\',
+		r'\\')
+	assert params == '{"type":1,"message":"Cannot save log to ${expected_err_path}. Saving log to $expected_alt_log_path"}'
 }
 
 fn init_tests() (&test_utils.Testio, server.Vls) {
