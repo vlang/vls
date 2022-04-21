00a0f96e2334202c859c1c63bb1b87a117902f25
Jakub Jabłoński <jakub.jablonski@outfittery.de>
2022-04-21 19:26:07 +0200

    Do not decode json for now

--- server/code_lens.v
+++ server/code_lens.v
@@ -8,11 +8,10 @@ fn (mut ls Vls) code_lens(id string, params string) {
 		return
 	}
 
-	// code_lens_params :=
-	json.decode(lsp.CodeLensParams, params) or {
-		ls.panic(err.msg())
-		return
-	}
+	// json.decode(lsp.CodeLensParams, params) or {
+	// 	ls.panic(err.msg())
+	// 	return
+	// }
 
 	// TODO: compute codelenses, for now return empty result
 	ls.send_null(id)
