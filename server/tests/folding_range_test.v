d5aa1ad8155a089ddb038649f69eb301212ce5de
Ned Palacios <npdoesmc@gmail.com>
2022-05-17 17:56:33 +0800

    server/tests: fix folding_range_test.v

--- server/tests/folding_range_test.v
+++ server/tests/folding_range_test.v
@@ -1,8 +1,7 @@
 import server
 import test_utils
-import json
+import jsonrpc.server_test_utils { new_test_client }
 import lsp
-import os
 
 // TODO: enable top level comments support in the future
 const folding_range_results = {
@@ -49,55 +48,53 @@ const folding_range_results = {
 	]
 }
 
-fn test_folding_range() {
-	mut io := test_utils.Testio{
+fn test_folding_range() ? {
+	mut t := &test_utils.Tester{
 		test_files_dir: test_utils.get_test_files_path(@FILE)
+		folder_name: 'folding_range'
+		client: new_test_client(server.new())
 	}
-	mut ls := server.new(io)
-	ls.dispatch(io.request_with_params('initialize', lsp.InitializeParams{
-		root_uri: lsp.document_uri_from_path(os.join_path(os.dir(@FILE), 'test_files',
-			'folding_range'))
-	}))
-	test_files := io.load_test_file_paths('folding_range') or {
-		io.bench.fail()
-		eprintln(io.bench.step_message_fail(err.msg()))
-		// assert false
-		return
+
+	test_files := t.initialize() or {
+		// TODO: skip for now
+		if err.msg() == 'no test files found for "folding_range"' {
+			return
+		}
+		return err
 	}
-	io.bench.set_total_expected_steps(test_files.len)
-	for test_file_path in test_files {
-		io.bench.step()
-		test_name := os.base(test_file_path)
+
+	for file in test_files {
+		test_name := file.file_name
 		err_msg := if test_name !in folding_range_results {
-			'missing results for $test_name'
+			'missing results'
 		} else {
 			''
 		}
 		if err_msg.len != 0 {
-			io.bench.fail()
-			eprintln(io.bench.step_message_fail(err_msg))
-			continue
-		}
-		content := os.read_file(test_file_path) or {
-			io.bench.fail()
-			eprintln(io.bench.step_message_fail('file $test_file_path is missing'))
+			t.fail(file, err_msg)
 			continue
 		}
 		// open document
-		req, doc_id := io.open_document(test_file_path, content)
-		ls.dispatch(req)
+		doc_id := t.open_document(file) or {
+			t.fail(file, err.msg())
+			continue
+		}
+
 		// initiate folding range request
-		ls.dispatch(io.request_with_params('textDocument/foldingRange', lsp.FoldingRangeParams{
+		actual := t.client.send<lsp.FoldingRangeParams, []lsp.FoldingRange>('textDocument/foldingRange', lsp.FoldingRangeParams{
 			text_document: doc_id
-		}))
+		}) ?
+
 		// compare content
-		println(io.bench.step_message('Testing $test_file_path'))
-		assert io.result() == json.encode(folding_range_results[test_name])
+		assert actual == folding_range_results[test_name]
+
 		// Delete document
-		ls.dispatch(io.close_document(doc_id))
-		io.bench.ok()
-		println(io.bench.step_message_ok(test_name))
+		t.close_document(doc_id) or {
+			t.fail(file, err.msg())
+			continue
+		}
+
+		t.ok(file)
 	}
-	assert io.bench.nfail == 0
-	io.bench.stop()
+	assert t.is_ok()
 }
