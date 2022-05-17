ca9aa286573c7d81ee5e002327d8ba94825b76fe
Ned Palacios <npdoesmc@gmail.com>
2022-05-17 15:46:30 +0800

    server: fix window_test

--- server/window_test.v
+++ server/window_test.v
@@ -1,6 +1,7 @@
 import server
 import lsp
 import test_utils
+import jsonrpc.server_test_utils { new_test_client, TestClient }
 import json
 
 const (
@@ -8,147 +9,92 @@ const (
 	show_message_method = 'window/showMessage'
 )
 
-fn test_log_message_error() {
-	mut io := &test_utils.Testio{}
-	mut ls := server.new(io)
+fn new_client() (&TestClient, server.ResponseWriter) {
+	client := new_test_client(server.new())
+	return client, client.server.writer()
+}
+
+fn test_log_message_error() ? {
+	io, mut wr := new_client()
 	// error
 	wr.log_message('Error!', .error)
-	method, params := io.notification() or {
-		assert false
-		return
-	}
-	assert method == log_message_method
-	assert params == json.encode(lsp.LogMessageParams{
-		@type: .error
-		message: 'Error!'
-	})
+	msg := io.stream.notification_at<lsp.LogMessageParams>(0) ?
+	assert msg.method == log_message_method
+	assert msg.params == lsp.LogMessageParams{.error, 'Error!'}
 }
 
-fn test_log_message_warning() {
-	mut io := &test_utils.Testio{}
-	mut ls := server.new(io)
+fn test_log_message_warning() ? {
+	io, mut wr := new_client()
 	// warning
 	wr.log_message('This is a warning!', .warning)
-	method, params := io.notification() or {
-		assert false
-		return
-	}
-	assert method == log_message_method
-	assert params == json.encode(lsp.LogMessageParams{
-		@type: .warning
-		message: 'This is a warning!'
-	})
+	msg := io.stream.notification_at<lsp.LogMessageParams>(0) ?
+	assert msg.method == log_message_method
+	assert msg.params == lsp.LogMessageParams{.warning, 'This is a warning!'}
 }
 
-fn test_log_message_info() {
-	mut io := &test_utils.Testio{}
-	mut ls := server.new(io)
+fn test_log_message_info() ? {
+	io, mut wr := new_client()
 	// info
 	wr.log_message('Hello World!', .info)
-	method, params := io.notification() or {
-		assert false
-		return
-	}
-	assert method == log_message_method
-	assert params == json.encode(lsp.LogMessageParams{
-		@type: .info
-		message: 'Hello World!'
-	})
+	msg := io.stream.notification_at<lsp.LogMessageParams>(0) ?
+	assert msg.method == log_message_method
+	assert msg.params == lsp.LogMessageParams{.info, 'Hello World!'}
 }
 
-fn test_log_message_log() {
-	mut io := &test_utils.Testio{}
-	mut ls := server.new(io)
+fn test_log_message_log() ? {
+	io, mut wr := new_client()
 	// log
 	wr.log_message('Logged!', .log)
-	method, params := io.notification() or {
-		assert false
-		return
-	}
-	assert method == log_message_method
-	assert params == json.encode(lsp.LogMessageParams{
-		@type: .log
-		message: 'Logged!'
-	})
+	msg := io.stream.notification_at<lsp.LogMessageParams>(0) ?
+	assert msg.method == log_message_method
+	assert msg.params == lsp.LogMessageParams{.log, 'Logged!'}
 }
 
-fn test_show_message_error() {
-	mut io := &test_utils.Testio{}
-	mut ls := server.new(io)
+fn test_show_message_error() ? {
+	io, mut wr := new_client()
 	// error
 	wr.show_message('Error!', .error)
-	method, params := io.notification() or {
-		assert false
-		return
-	}
-	assert method == show_message_method
-	assert params == json.encode(lsp.ShowMessageParams{
-		@type: .error
-		message: 'Error!'
-	})
+	msg := io.stream.notification_at<lsp.ShowMessageParams>(0) ?
+	assert msg.method == show_message_method
+	assert msg.params == lsp.ShowMessageParams{.error, 'Error!'}
 }
 
-fn test_show_message_warning() {
-	mut io := &test_utils.Testio{}
-	mut ls := server.new(io)
+fn test_show_message_warning() ? {
+	io, mut wr := new_client()
 	// warning
 	wr.show_message('This is a warning!', .warning)
-	method, params := io.notification() or {
-		assert false
-		return
-	}
-	assert method == show_message_method
-	assert params == json.encode(lsp.ShowMessageParams{
-		@type: .warning
-		message: 'This is a warning!'
-	})
+	msg := io.stream.notification_at<lsp.ShowMessageParams>(0) ?
+	assert msg.method == show_message_method
+	assert msg.params == lsp.ShowMessageParams{.warning, 'This is a warning!'}
 }
 
-fn test_show_message_info() {
-	mut io := &test_utils.Testio{}
-	mut ls := server.new(io)
+fn test_show_message_info() ? {
+	io, mut wr := new_client()
 	// info
 	wr.show_message('Hello World!', .info)
-	method, params := io.notification() or {
-		assert false
-		return
-	}
-	assert method == show_message_method
-	assert params == json.encode(lsp.ShowMessageParams{
-		@type: .info
-		message: 'Hello World!'
-	})
+	msg := io.stream.notification_at<lsp.ShowMessageParams>(0) ?
+	assert msg.method == show_message_method
+	assert msg.params == lsp.ShowMessageParams{.info, 'Hello World!'}
 }
 
-fn test_show_message_log() {
-	mut io := &test_utils.Testio{}
-	mut ls := server.new(io)
+fn test_show_message_log() ? {
+	io, mut wr := new_client()
 	// log
 	wr.show_message('Logged!', .log)
-	method, params := io.notification() or {
-		assert false
-		return
-	}
-	assert method == show_message_method
-	assert params == json.encode(lsp.ShowMessageParams{
-		@type: .log
-		message: 'Logged!'
-	})
+	msg := io.stream.notification_at<lsp.ShowMessageParams>(0) ?
+	assert msg.method == show_message_method
+	assert msg.params == lsp.ShowMessageParams{.log, 'Logged!'}
 }
 
-fn test_show_message_request() {
-	mut io := &test_utils.Testio{}
-	mut ls := server.new(io)
+fn test_show_message_request() ? {
+	io, mut wr := new_client()
 	actions := [lsp.MessageActionItem{'Retry'}]
 	wr.show_message_request('Failed!', actions, .info)
-	method, params := io.notification() or {
-		assert false
-		return
-	}
-	assert method == 'window/showMessageRequest'
-	assert params == json.encode(lsp.ShowMessageRequestParams{
+	msg := io.stream.notification_at<lsp.ShowMessageRequestParams>(0) ?
+	assert msg.method == 'window/showMessageRequest'
+	assert msg.params == lsp.ShowMessageRequestParams{
 		@type: .info
 		message: 'Failed!'
 		actions: actions
-	})
+	}
 }
