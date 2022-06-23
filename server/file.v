689f3496febb44065e881d78de5663f51ba29926
Ned Palacios <npdoesmc@gmail.com>
2022-06-23 23:45:29 +0800

    server: migrate to ropes, remove array manipulation in did_change

--- server/file.v
+++ server/file.v
@@ -2,21 +2,22 @@ module server
 
 import ast
 import lsp
+import ropes
 
 struct File {
 mut:
 	uri     lsp.DocumentUri
-	source  []rune
+	source  &ropes.Rope
 	tree    &ast.Tree [required]
 	version int = 1
 }
 
 fn (file &File) get_offset(line int, col int) int {
-	return compute_offset(file.source, line, col)
+	return compute_offset(file.source.runes(), line, col)
 }
 
 fn (file &File) get_position(offset int) lsp.Position {
-	return compute_position(file.source, offset)
+	return compute_position(file.source.runes(), offset)
 }
 
 fn (file_map map[string]File) count(dir string) int {
