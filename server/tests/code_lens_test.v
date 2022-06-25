7d2a61adea4bf4d5ac161435550d06693afdd397
Ned Palacios <npdoesmc@gmail.com>
2022-06-25 12:13:03 +0800

    server/tests: add textDocument/codeLens test

---
+++ server/tests/code_lens_test.v
@@ -0,0 +1,41 @@
+import server
+import test_utils
+import jsonrpc.server_test_utils { new_test_client }
+import lsp
+
+fn test_code_lens() ? {
+	mut ls := server.new()
+	mut t := &test_utils.Tester{
+		test_files_dir: test_utils.get_test_files_path(@FILE)
+		folder_name: 'code_lens'
+		client: new_test_client(ls)
+	}
+
+	mut writer := t.client.server.writer()
+	test_files := t.initialize() ?
+	for file in test_files {
+		// open document
+		doc_id := t.open_document(file) or {
+			t.fail(file, err.msg())
+			continue
+		}
+
+		if _ := ls.code_lens(lsp.CodeLensParams{
+			text_document: doc_id
+		}, mut writer) {
+			assert false
+		} else {
+			assert err is none
+
+			// Delete document
+			t.close_document(doc_id) or {
+				t.fail(file, err.msg())
+				continue
+			}
+
+			t.ok(file)
+		}
+	}
+
+	assert t.is_ok()
+}
