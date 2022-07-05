83cdeb5fe141e951ad0ce887b094705e3326b41d
Ned Palacios <npdoesmc@gmail.com>
2022-07-05 23:19:54 +0800

    analyzer: use tree_cursor for delete_symbol_at_node

--- analyzer/ast.v
+++ analyzer/ast.v
@@ -1,6 +1,5 @@
 module analyzer
 
-import ast
 import tree_sitter_v as v
 import datatypes { Stack }
 
@@ -9,67 +8,3 @@ fn within_range(node_range C.TSRange, start_line u32, end_line u32) bool {
 		|| (node_range.end_point.row >= start_line && node_range.end_point.row <= end_line)
 }
 
-const excluded_nodes = [v.NodeType.const_declaration, .global_var_declaration]
-
-const included_nodes = [v.NodeType.const_spec, .global_var_spec, .global_var_type_initializer, .block]
-
-struct NodeIterAttr {
-	cur_node    ast.Node
-	child_count u32
-	cur_idx     u32
-}
-
-struct RangeNodeIterator {
-mut:
-	cur_node    ast.Node
-	child_count u32
-	cur_idx     u32
-	start_line  u32
-	end_line    u32
-	stack       Stack<NodeIterAttr>
-}
-
-fn (mut iter RangeNodeIterator) next() ?ast.Node {
-	if iter.cur_idx >= iter.child_count {
-		if last_attr := iter.stack.pop() {
-			iter.cur_node = last_attr.cur_node.next_named_sibling() or { return none }
-			iter.child_count = last_attr.child_count
-			iter.cur_idx = last_attr.cur_idx
-			return iter.next()
-		}
-		return none
-	}
-	child := if iter.cur_idx == 0 {
-		iter.cur_node.named_child(0) or { return none }
-	} else {
-		iter.cur_node.next_named_sibling() or { return none }
-	}
-	type_name := child.type_name
-	if ((type_name.is_declaration() && type_name !in analyzer.excluded_nodes)
-		|| type_name in analyzer.included_nodes) && within_range(child.range(), iter.start_line, iter.end_line) {
-		defer {
-			iter.cur_node = child
-			iter.cur_idx++
-		}
-		return child
-	} else {
-		iter.stack.push(NodeIterAttr{
-			cur_node: iter.cur_node
-			child_count: iter.child_count
-			cur_idx: iter.cur_idx
-		})
-		iter.cur_node = child
-		iter.child_count = child.named_child_count()
-		iter.cur_idx = 0
-		return iter.next()
-	}
-}
-
-fn get_nodes_within_range(node ast.Node, start_line u32, end_line u32) &RangeNodeIterator {
-	return &RangeNodeIterator{
-		cur_node: node
-		child_count: node.named_child_count()
-		start_line: start_line
-		end_line: end_line
-	}
-}
--- analyzer/store.v
+++ analyzer/store.v
@@ -239,8 +239,8 @@ pub fn (mut ss Store) register_symbol(mut info Symbol) ?&Symbol {
 
 		// Remove this?
 		if existing_sym.kind !in analyzer.container_symbol_kinds  {
-			if existing_sym.kind != .placeholder 
-				&& (info.range.start_point.row > existing_sym.range.start_point.row 
+			if existing_sym.kind != .placeholder
+				&& (info.range.start_point.row > existing_sym.range.start_point.row
 				|| (existing_sym.kind == info.kind
 				&& (existing_sym.file_path == info.file_path
 				&& existing_sym.file_version >= info.file_version))) {
@@ -893,14 +893,14 @@ pub fn (mut ss Store) infer_value_type_from_node(node ast.Node, src_text tree_si
 
 // delete_symbol_at_node removes a specific symbol from a specific portion of the node
 pub fn (mut ss Store) delete_symbol_at_node(root_node ast.Node, src tree_sitter.SourceText, start_line u32, end_line u32) bool {
-	unsafe { ss.opened_scopes[ss.cur_file_path].free() }
-	mut nodes := get_nodes_within_range(root_node, start_line, end_line)
-
+	// remove by scope
 	ss.opened_scopes[ss.cur_file_path].remove_symbols_by_line(start_line, end_line)
 
-	for node in nodes {
-		node_type_name := node.type_name
-		match node_type_name {
+	// remove by node
+	mut cursor := new_tree_cursor(root_node, start_line_nr: start_line)
+
+	for node in cursor {
+		match node.type_name {
 			.const_spec, .global_var_spec, .global_var_declaration, .function_declaration,
 			.interface_declaration, .enum_declaration, .type_declaration, .struct_declaration {
 				name_node := node.child_by_field_name('name') or { continue }
@@ -915,20 +915,17 @@ pub fn (mut ss Store) delete_symbol_at_node(root_node ast.Node, src tree_sitter.
 							ss.binded_symbol_locations.delete(binded_location_idx)
 						}
 					}
-					if node_type_name == .function_declaration {
+					if node.type_name == .function_declaration {
 						ss.symbols[ss.cur_dir][idx].scope.remove_symbols_by_line(start_line, end_line)
 					}
 					ss.symbols[ss.cur_dir].delete(idx)
 				}
 
-				// if node_type_name in [.const_spec, .global_var_spec, .global_var_declaration] {
+				// if node.type_name in [.const_spec, .global_var_spec, .global_var_declaration] {
 				// 	mut innermost := ss.opened_scopes[ss.cur_file_path].innermost(node.start_byte(),
 				// 		node.end_byte())
 				// 	innermost.remove(symbol_name)
 				// }
-			}
-			.short_var_declaration {
-				ss.opened_scopes[ss.cur_file_path].remove_symbols_by_line(start_line, end_line)
 			}
 			.import_declaration {
 				mut imp_module := ss.imports.find_by_position(ss.cur_file_path, node.range()) or { continue }
