574eeb0aa57f812e43f0315014cd07bb3cf4f1e6
Ned Palacios <npdoesmc@gmail.com>
2022-07-19 14:53:58 +0800

    server: fix general_test, move debug logging info after vls info

--- server/general.v
+++ server/general.v
@@ -94,8 +94,6 @@ pub fn (mut ls Vls) initialize(params lsp.InitializeParams, mut wr ResponseWrite
 		}
 	}
 
-	wr.log_message('is_logger_installed: $is_logger_installed | is_logger_enabled: $is_logger_enabled | params.trace: ${params.trace}\n', .info)
-
 	// Create the file either in debug mode or when the client trace is set to verbose.
 	if is_logger_installed && is_logger_enabled {
 		// set up logger set to the workspace path
@@ -104,6 +102,9 @@ pub fn (mut ls Vls) initialize(params lsp.InitializeParams, mut wr ResponseWrite
 
 	// print initial info
 	ls.print_info(params.process_id, params.client_info, mut wr)
+
+	// print debug options info to avoid general_test failing
+	wr.log_message('is_logger_installed: $is_logger_installed | is_logger_enabled: $is_logger_enabled | params.trace: ${params.trace}', .info)
 
 	// since builtin is used frequently, they should be parsed first and only once
 	analyzer.setup_builtin(mut ls.store, os.join_path(ls.vroot_path, 'vlib', 'builtin'))
