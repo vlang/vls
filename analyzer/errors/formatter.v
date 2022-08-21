e0885ca5392355bff5f2f87b934bbe6cb6a21282
Ned Palacios <npdoesmc@gmail.com>
2022-08-21 22:50:03 +0800

    analyzer/errors: add support for error templates with named variables

--- analyzer/errors/formatter.v
+++ analyzer/errors/formatter.v
@@ -21,6 +21,9 @@ pub fn format(data_formatter_fn DataFormatterFn, code_or_msg string, datum ...Er
 	defer { unsafe { scanner.free() } }
 
 	mut builder := strings.new_builder(error_template.len)
+	mut var_name := strings.new_builder(100)
+	defer { unsafe { var_name.free() } }
+
 	mut cur_data_idx := 0
 	for scanner.remaining() != 0 {
 		chr := scanner.next()
@@ -29,6 +32,33 @@ pub fn format(data_formatter_fn DataFormatterFn, code_or_msg string, datum ...Er
 			cur_data_idx++
 
 			scanner.skip_n(1)
+		} else if chr == `{` && scanner.peek() == chr && (datum.last() is map[string]ErrorData || datum.last() is map[string]string) {
+			// maps are used for accepting named parameters in error messages
+			// e.g. "cannot selectively import {{var}} from {{mod}}. use {{mod}}.{{var}} instead"
+			scanner.skip_n(1)
+
+			// get var name
+			for scanner.current() != `}` && scanner.peek() != `}` {
+				var_chr := scanner.next()
+				if var_chr == ` ` {
+					continue
+				}
+
+				var_name.write_rune(var_chr)
+			}
+
+			// get data
+			last := datum.last()
+			got_var_name := var_name.str()
+			if last is map[string]ErrorData {
+				value := (*last)[got_var_name] or { ErrorData('missing value') }
+				builder.write_string(data_formatter_fn(value))
+			} else if last is map[string]string {
+				value := (*last)[got_var_name] or { 'missing value' }
+				builder.write_string(value)
+			}
+
+			scanner.skip_n(2)
 		} else {
 			// TODO: maps
 			builder.write_rune(chr)
--- analyzer/errors/formatter_test.v
+++ analyzer/errors/formatter_test.v
@@ -12,3 +12,7 @@ fn test_format() {
 	assert errors.format(simple_data_formatter, 'hello %s', 'world') == 'hello world'
 	assert errors.format(simple_data_formatter, errors.undefined_operation_error, 'int', '+', 'int') == 'undefined operation `int` + `int`'
 }
+
+fn test_format_map() {
+	assert errors.format(simple_data_formatter, errors.selective_const_import_error, {'var': 'foo', 'module': 'modz'}) == 'cannot selective import constant `foo` from `modz`, import `modz` and use `modz.foo` instead'
+}
